7a5991f7fbd8d9b36dda558c6401ae72721b59d8
dayana <dayana82006@gmail.com>
2025-01-22 13:28:11 -0500

    feat: :sparkles: update

--- components/header/headerComponents.js
+++ components/header/headerComponents.js
@@ -1,45 +1,55 @@
-/*import '../../../Models/userModel.js';*/
 export class HeaderComponent extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({mode: 'open'});
-        this.shadowRoot.innerHTML = /*html*/`
-        <style>
-        @import url('../components/header/styleHeader.css');
-      </style>
-        <div class="card-body">
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
 
-        <div class="row">
-          <div class="col-6">
-            <h2 class="card-title">Registro Usuario</h2>
-          </div>
-          <div-col-6>
-            <p id= "nroFactura">Nro factura: </p>
-          </div-col-6>
-        </div>
-        <form>
-          <div class="row">
-            <div class="col-6">
-              <label class="form-label">Nro Id: </label>
-              <input class="form-control" type="text" name="idUsuario">
+    // Crear el link para importar el CSS
+    const style = document.createElement('link');
+    style.rel = 'stylesheet';
+    style.href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css'; // O tu archivo CSS local
 
-              <label for="nameuser" class="form-label">Nombre: </label>
-              <input type="email" class="form-control" name="nameuser" id="nameuser">
+    // Agregar la plantilla HTML del componente
+    const template = document.createElement('template');
+    template.innerHTML = /*html*/ `
 
+    <style>
+      h2{
+        color: rgb(77, 84, 148);
+        text-shadow: 2px 4px 4px rgb(196, 196, 196);
+      }
+    </style>
+      <div class="container">
+        <div class="card">
+          <div class="card-body">
+            <div class="row">
+              <div class="col-6">
+                <h2 class="card-title">Registro Usuario</h2>
+              </div>
+              <div class="col-6">
+                <p id="nroFactura">Nro factura: </p>
+              </div>
             </div>
-           
-      
-        </form>
-        
+            <form>
+              <div class="row">
+                <div class="col-6">
+                  <label class="form-label">Nro Id:</label>
+                  <input class="form-control" type="text" name="idUsuario">
+                  <label for="nameuser" class="form-label">Nombre:</label>
+                  <input class="form-control" type="text" name="nameuser" id="nameuser">
+                  <label for="exampleInputEmail1" class="form-label">Email address:</label>
+                  <input class="form-control" type="email" id="exampleInputEmail1">
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
-        `;
-    }
-    connectedCallback() {
+    `;
 
-    }
-
-    
-    
-    
+    // Agregar el CSS y la plantilla al Shadow DOM
+    this.shadowRoot.appendChild(style);
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
 }
-customElements.define("header-component",HeaderComponent);
+
+customElements.define('header-component', HeaderComponent);
